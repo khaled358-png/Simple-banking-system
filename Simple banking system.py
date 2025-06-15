480f391a1fd4596b50f4205b2e6df0affb941d05
Khaled Gamal Helal <Khaled.Helal@andalusiagroup.net>
2025-06-15 14:29:44 +0300

    Simple banking System

---
+++ Simple banking system.py
@@ -0,0 +1,37 @@
+accounts = {
+    "khaled": {"email": "khaled@example.com", "balance": 1},
+    "gamal": {"email": "gamal@example.com", "balance": 2},
+    "helal": {"email": "helal@example.com", "balance": 3}
+}
+
+
+name = input("Enter your name: ")
+
+if name in accounts:
+    print("Choose an operation:")
+    print("1. View Balance")
+    print("2. Deposit Money")
+    print("3. Withdraw Money")
+
+    choice = input("Chooes  number: ")
+
+    if choice == "1":
+        print("Your current balance is:", accounts[name]["balance"])
+
+    elif choice == "2":
+        amount = float(input("Enter amount to deposit: "))
+        accounts[name]["balance"] += amount
+        print("Deposit successful. New balance:", accounts[name]["balance"])
+
+    elif choice == "3":
+        amount = float(input("Enter amount to withdraw: "))
+        if amount <= accounts[name]["balance"]:
+            accounts[name]["balance"] -= amount
+            print("Withdrawal successful. New balance:", accounts[name]["balance"])
+        else:
+            print("Insufficient balance.")
+
+    else:
+        print("Invalid choice.")
+else:
+    print("User not found.")
